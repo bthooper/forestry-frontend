387752efb1a74a11c828dbf8746a7b9c79177da2
Bryan Hooper <bthooper@gmail.com>
2020-06-10 17:19:10 -0400

    added needed libraries and set up imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redxu';
 
 import App from './App';
 
 ReactDOM.render(
-    <App />
+    <App />,
   document.getElementById('root')
 );
 
